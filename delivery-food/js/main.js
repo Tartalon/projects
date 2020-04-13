fc4a8d1e1807bb8ee4be79184fa54ed5c1ac2f66
Vladimir <tartalon23@gmail.com>
2020-04-13 19:30:27 +0300

    added noscroll on body modal is open

--- delivery-food/js/main.js
+++ delivery-food/js/main.js
@@ -1,14 +1,21 @@
 const cartButton = document.querySelector('#cart-button');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
+let body = document.querySelector('#body');
 
-cartButton.addEventListener('click', function (event) {
+cartButton.onclick = function () {
   modal.classList.add('is-open');
-  body.style.overflow = hidden;
-});
-close.addEventListener('click', function (event) {
+  body.classList.add('scroll');
+}
+close.onclick = function () {
   modal.classList.remove('is-open');
-});
-
+  body.classList.remove('scroll');
+}
+window.onclick = function (e) {
+  if(e.target == modal) {
+    modal.classList.remove('is-open');
+    body.classList.remove('scroll');
+  }
+}
 
 new WOW().init();
