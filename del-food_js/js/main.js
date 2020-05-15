551d90a3d257773d35a7da38e658de378ad93e97
Vladimir <tartalon23@gmail.com>
2020-05-15 17:23:18 +0300

    food basket

--- del-food_js/js/main.js
+++ del-food_js/js/main.js
@@ -31,9 +31,15 @@ const rating = document.querySelector('.rating');
 const minPrice = document.querySelector('.price');
 const category = document.querySelector('.category');
 const inputSearch = document.querySelector('.input-search');
+const modalBody = document.querySelector('.modal-body');
+const modalPrice = document.querySelector('.modal-pricetag');
+const buttonClearCart = document.querySelector('.clear-cart');
 
 // создаем переменную login
 let login = localStorage.getItem('gloDelivery');
+
+// массив для корзины
+const cart = [];
 
 //валидация формы. Регулярные выражения пишуться между слешами
 const valid = function (str) {
@@ -85,8 +91,6 @@ function returnMain() {
 
 // функция если пользователь авторизован
 function authorized() {
-  console.log('Авторизован');
-
   //функция выхода
   function logOut() {
     login = null;
@@ -98,10 +102,13 @@ function authorized() {
     buttonAuth.style.display = '';
     userName.style.display = '';
     buttonOut.style.display = '';
+    cartButton.style.display = '';
     buttonOut.removeEventListener('click', logOut);
     checkAuth();
     returnMain();
   }
+
+  console.log('Авторизован');
 
   //если авторизован скрываем кнопку "войти"
   buttonAuth.style.display = 'none';
@@ -111,7 +118,8 @@ function authorized() {
 
   //если авторизован показываем логин и кнопку выхода
   userName.style.display = 'inline'; //inline т.к. <span>
-  buttonOut.style.display = 'block';
+  buttonOut.style.display = 'flex';
+  cartButton.style.display = 'flex';
 
   // на кнопку выхода навешиваем событие при котором запуститься функция выхода
   buttonOut.addEventListener('click', logOut);
@@ -214,7 +222,7 @@ function createCardRestaurant(restaurant) {
 }
 
 // функция создания карточки в меню. Деструктурируем
-function createCardGood({ description, image, name, price }) {
+function createCardGood({ description, image, name, price, id }) {
   // создаем div
   const card = document.createElement('div');
   // задаем класс для дива
@@ -236,11 +244,11 @@ function createCardGood({ description, image, name, price }) {
       </div>
       <!-- /.card-info -->
       <div class="card-buttons">
-        <button class="button button-primary button-add-cart">
+        <button class="button button-primary button-add-cart" id="${id}">
           <span class="button-card-text">В корзину</span>
           <span class="button-cart-svg"></span>
         </button>
-        <strong class="card-price-bold">${price} ₽</strong>
+        <strong class="card-price card-price-bold">${price} ₽</strong>
       </div>
     </div>
     <!-- /.card-text -->
@@ -296,13 +304,105 @@ function openGoods(event) {
   }
 }
 
+//отслеживаем клик по карточке для попадания товара в корзину
+function addToCart(event) {
+  const target = event.target;
+  const buttonAddToCart = target.closest('.button-add-cart');
+
+  if (buttonAddToCart) {
+    const card = target.closest('.card');
+    const title = card.querySelector('.card-title-reg').textContent;
+    const cost = card.querySelector('.card-price').textContent;
+    const id = buttonAddToCart.id;
+
+    // find - поиск по указанному условию
+    const food = cart.find(function (item) {
+      return item.id === id;
+    });
+
+    if (food) {
+      food.count += 1;
+    } else {
+      // получаем значения в массив cart в виде объекта
+      cart.push({
+        id, //id: id,
+        title, //title: title,
+        cost, //cost: cost,
+        count: 1,
+      });
+    }
+
+    console.log(cart);
+  }
+}
+
+function renderCart() {
+  modalBody.textContent = ''; // когда мы кликаем на корзине она сначала очищается
+
+  cart.forEach(function ({ id, title, cost, count }) {
+    const itemCart = `
+      <div class="food-row">
+        <span class="food-name">${title}</span>
+        <strong class="food-price">${cost}</strong>
+        <div class="food-counter">
+          <button class="counter-button counter-minus" data-id=${id}>-</button>
+          <span class="counter">${count}</span>
+          <button class="counter-button counter-plus" data-id=${id}>+</button>
+        </div>
+      </div>
+    `;
+
+    modalBody.insertAdjacentHTML('beforeend', itemCart);
+  });
+
+  const totalPrice = cart.reduce(function (result, item) {
+    return result + parseFloat(item.cost) * item.count;
+  }, 0);
+
+  modalPrice.textContent = totalPrice + ' ₽';
+}
+
+// функция изменения количества товаров при клике на + и -.
+function changeCount(event) {
+  const target = event.target;
+
+  if (target.classList.contains('counter-button')) {
+    const food = cart.find(function (item) {
+      return item.id === target.dataset.id;
+    });
+
+    if (target.classList.contains('counter-minus')) {
+      food.count--;
+      if (food.count === 0) {
+        cart.splice(cart.indexOf(food), 1);
+      }
+    }
+    if (target.classList.contains('counter-plus')) food.count++; //если одна опирация то можно записать в одну строку без {}.
+
+    renderCart(); // перезапуск
+  }
+}
+
 //функция инициализации (запуска) проекта
 function init() {
   getData('./db/partners.json').then(function (data) {
     data.forEach(createCardRestaurant); //увидем 6 карточек тануки на странице
   }); // в then указываем кол-бек функцию кoтороя выполниться после того как нам вернется ответ с сервера. при помощи then обрабатываются промисы
 
-  cartButton.addEventListener('click', toggleModal);
+  cartButton.addEventListener('click', function () {
+    renderCart();
+    toggleModal();
+  });
+
+  // кнопка отмены в корзине
+  buttonClearCart.addEventListener('click', function () {
+    cart.length = 0;
+    renderCart();
+  });
+
+  modalBody.addEventListener('click', changeCount);
+
+  cardsMenu.addEventListener('click', addToCart);
 
   close.addEventListener('click', toggleModal);
 
