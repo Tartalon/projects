11fba7475eb30b9ab5a433d596b8b81e54199e2a
Vladimir <tartalon23@gmail.com>
2020-05-20 13:38:16 +0300

    modal input phone + setTimeout on close

--- quiz/script/script.js
+++ quiz/script/script.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const nextButton = document.getElementById('next');
   const prevButton = document.getElementById('prev');
   const modalDialog = document.querySelector('.modal-dialog');
+  const sendButton = document.getElementById('send');
 
   // объект в котором вопросы и ответы
   const questions = [
@@ -164,6 +165,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // запуск текста
   const playTest = () => {
+    //объект куда будут складываться ответы
+    const finelAnswers = [];
+
     let numberQuestion = 0;
 
     // рендерим отвуты
@@ -171,13 +175,17 @@ document.addEventListener('DOMContentLoaded', function () {
       questions[index].answers.forEach(answer => {
         const answerItem = document.createElement('div');
 
-        answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+        answerItem.classList.add(
+          'answers-item',
+          'd-flex',
+          'justify-content-center'
+        );
 
         answerItem.innerHTML = `
           <input type = "${questions[index].type}"
           id = "${answer.title}"
           name = "answer"
-          class = "d-none">
+          class = "d-none" value="${answer.title}">
           <label
           for = "${answer.title}"
           class = "d-flex flex-column justify-content-between" >
@@ -209,21 +217,76 @@ document.addEventListener('DOMContentLoaded', function () {
 
       if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
         questionTitle.textContent = `${questions[indexQuestion].question}`;
-
         renderAnswers(indexQuestion);
+
+        nextButton.classList.remove('d-none');
+        prevButton.classList.remove('d-none');
+        sendButton.classList.add('d-none');
+      }
+
+      if (numberQuestion === 0) {
+        prevButton.classList.add('d-none');
+      }
+
+      if (numberQuestion === questions.length) {
+        nextButton.classList.add('d-none');
+        prevButton.classList.add('d-none');
+        sendButton.classList.remove('d-none');
+
+        formAnswers.innerHTML = `
+        <div class="form-group">
+          <label for="numberPhone">Ваш номер телефона?</label>
+          <input type="phone" class="form-control" id="numberPhone">
+        </div>
+        `;
+      }
+
+      if (numberQuestion === questions.length + 1) {
+        formAnswers.textContent = 'Спасибо за пройденный тест!';
+        setTimeout(() => {
+          modalBlock.classList.remove('d-block');
+        }, 2000);
       }
     };
 
     // запуск рендеринга
     renderQuestions(numberQuestion);
 
+    const checkAnswer = () => {
+      const obj = {};
+      const inputs = [...formAnswers.elements].filter(
+        input => input.checked || input.id === 'numberPhone'
+      );
+      // console.log(inputs);
+
+      inputs.forEach((input, index) => {
+        if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+          obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+        }
+
+        if (numberQuestion === questions.length) {
+          obj['Номер телефона'] = input.value;
+        }
+      });
+
+      finelAnswers.push(obj);
+      console.log(finelAnswers);
+    };
+
     // на onclick обработчик вешается только раз при клике, а при addEventListener при каждом клике  плюсуется
     nextButton.onclick = () => {
+      checkAnswer();
       numberQuestion++;
       renderQuestions(numberQuestion);
     };
     prevButton.onclick = () => {
       numberQuestion--;
+      renderQuestions(numberQuestion);
+    };
+
+    sendButton.onclick = () => {
+      checkAnswer();
+      numberQuestion++;
       renderQuestions(numberQuestion);
     };
   };
