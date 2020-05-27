37044e978c3336f54f88b1c98fb910e5b203bfa0
Vladimir <tartalon23@gmail.com>
2020-05-27 13:36:56 +0300

    voteElem, backdropIMG

--- netclicks/script/main.js
+++ netclicks/script/main.js
@@ -8,54 +8,70 @@
 // const bar = function() {
 //   console.log('function expression'); // вызывается только после обьявления
 // }
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = 'f48226d3071e8f05afa99e20323348ef';
 
 //элементы
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
 const tvShowsList = document.querySelector('.tv-shows__list');
+const tvCardVote = document.querySelector('.tv-card__vote');
 const modal = document.querySelector('.modal');
 
 // классы; запрос на сервер
 const DBService = class {
-  getData = async (url) => {
+  getData = async url => {
     const res = await fetch(url);
     if (res.ok) {
       return res.json();
     } else {
-      throw new Error(`Не удалось получить данные по адресу ${url}`)
+      throw new Error(`Не удалось получить данные по адресу ${url}`);
     }
-  }
+  };
 
   getTestData = async () => {
-    return await this.getData('test.json')
-  }
-}
+    return await this.getData('test.json');
+  };
+};
 
 const renderCard = response => {
   console.log(response);
   tvShowsList.textContent = '';
 
   response.results.forEach(item => {
-    // console.log(item);
+    //деструктурируем item
+    const {
+      backdrop_path: backdrop,
+      name: title,
+      poster_path: poster,
+      vote_average: vote,
+    } = item;
+
+    const posterIMG = poster ? IMG_URL + poster : './img/no-poster.jpg';
+
+    // ДЗ если нет backdrop то не выводить его
+    // ДЗ если нет voteElem - не выводим span tv-card__vote
+    const backdropIMG = backdrop ? IMG_URL + backdrop : '';
+    const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
+
     const card = document.createElement('li');
     card.classList.add('tv-shows__item');
     card.innerHTML = `
       <a href="#" class="tv-card">
-        <span class="tv-card__vote">8.1</span>
+      ${voteElem}
         <img
           class="tv-card__img"
-          src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/o9XEd15zRzef9SlOuJYPdS5HCdK.jpg"
-          data-backdrop="https://image.tmdb.org/t/p/w185_and_h278_bestv2/lXWgVQ1whAEMz4Ju88UyPoveIKD.jpg"
-          alt="Звёздные войны: Войны клонов"
+          src="${posterIMG}"
+          data-backdrop="${backdropIMG}"
+          alt="Звёздные войны: ${title}"
         />
-        <h4 class="tv-card__head">Звёздные войны: Войны клонов</h4>
+        <h4 class="tv-card__head">Звёздные войны: ${title}</h4>
       </a>
     `;
 
     tvShowsList.append(card);
-
   });
-}
+};
 
 new DBService().getTestData().then(renderCard);
 
@@ -83,7 +99,6 @@ leftMenu.addEventListener('click', event => {
   }
 });
 
-
 // открытие модалки
 tvShowsList.addEventListener('click', event => {
   event.preventDefault();
@@ -91,16 +106,17 @@ tvShowsList.addEventListener('click', event => {
   const card = target.closest('.tv-card');
 
   if (card) {
-    document.body.style.overflow = 'hidden'; //отключили скрол бади 
+    document.body.style.overflow = 'hidden'; //отключили скрол бади
     modal.classList.remove('hide');
   }
 });
 
 // закрытие модалки
 modal.addEventListener('click', event => {
-
-  if (event.target.closest('.cross') ||
-    event.target.classList.contains('modal')) {
+  if (
+    event.target.closest('.cross') ||
+    event.target.classList.contains('modal')
+  ) {
     document.body.style.overflow = '';
     modal.classList.add('hide');
   }
@@ -123,10 +139,9 @@ const changeImage = event => {
   if (card) {
     const img = card.querySelector('.tv-card__img');
     if (img.dataset.backdrop) {
-      [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
+      [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src];
     }
   }
-
 };
 tvShowsList.addEventListener('mouseover', changeImage);
 tvShowsList.addEventListener('mouseout', changeImage);
