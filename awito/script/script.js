8979aaea41cafd27face71ec789e55ab47626008
tartalon <tartalon23@gmail.com>
2020-07-09 00:13:41 +0300

    get form elements in arr

--- awito/script/script.js
+++ awito/script/script.js
@@ -1,44 +1,74 @@
-'use strict';
+"use strict";
 
-const modalAdd = document.querySelector('.modal__add'),
-  addAd = document.querySelector('.add__ad'),
-  modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-  modalSubmit = document.querySelector('.modal__submit'),
-  catalog = document.querySelector('.catalog'),
-  modalItem = document.querySelector('.modal__item ');
+const modalAdd = document.querySelector(".modal__add"),
+  addAd = document.querySelector(".add__ad"),
+  modalBtnSubmit = document.querySelector(".modal__btn-submit"),
+  modalSubmit = document.querySelector(".modal__submit"),
+  catalog = document.querySelector(".catalog"),
+  modalItem = document.querySelector(".modal__item ");
 
+// Get form elements without button
+const elementsModalSubmit = [...modalSubmit.elements].filter(
+  (elem) => elem.tagName !== "BUTTON"
+);
+// console.log(
+//   [...elementsModalSubmit].filter((elem) => {
+//     return elem.tagName !== "BUTTON";
+//   })
+// );
+
+// Function close modal
+
+// const closeModal = (event) => {
+//   const target = event.target;
+
+//   if (
+//     target.classList.contains("modal__close") ||
+//     target === modalAdd ||
+//     target === modalItem
+//   ) {
+//     modalAdd.classList.add("hide");
+//     modalItem.classList.add("hide");
+//     modalSubmit.reset();
+//   }
+// };
+
+const closeModal = function (event) {
+  const target = event.target;
+  // console.log(this);
+
+  if (target.classList.contains("modal__close") || target === this) {
+    this.classList.add("hide");
+    if (this === modalItem) {
+      modalSubmit.reset();
+    }
+  }
+};
+
+// Function close modal on Escape
+const closeModalEsc = (event) => {
+  if (event.code === "Escape") {
+    modalItem.classList.add("hide");
+    modalAdd.classList.add("hide");
+    document.removeEventListener("keydown", closeModalEsc);
+  }
+};
 
 // show modal add card
-addAd.addEventListener('click', () => {
-  modalAdd.classList.remove('hide');
+addAd.addEventListener("click", () => {
+  modalAdd.classList.remove("hide");
   modalBtnSubmit.disabled = true;
+  document.addEventListener("keydown", closeModalEsc);
 });
 
+modalAdd.addEventListener("click", closeModal);
+modalItem.addEventListener("click", closeModal);
 
-modalAdd.addEventListener('click', event => {
-  // console.log(event);
+catalog.addEventListener("click", (event) => {
   const target = event.target;
 
-  if (target.classList.contains('modal__close') || target === modalAdd) {
-    modalAdd.classList.add('hide');
-    modalSubmit.reset();
+  if (target.closest(".card")) {
+    modalItem.classList.remove("hide");
+    document.addEventListener("keydown", closeModalEsc);
   }
-});
-
-catalog.addEventListener('click', event => {
-  const target = event.target.closest('.card');
-
-  if (target) {
-    modalItem.classList.remove('hide');
-    console.log('okk');
-  }
-});
-
-document.addEventListener('keydown', event => {
-  // console.log(event);
-  if (event.keyCode == 27) {
-    console.log('ok');
-    modalItem.classList.add('hide');
-  }
-  // console.log(target);
 });
