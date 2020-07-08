15012ec7cf0f2ae8968cda6eab010b7ae13bb240
Vladimir <50200804+Tartalon@users.noreply.github.com>
2020-07-08 17:34:10 +0300

    modal card

--- awito/script/script.js
+++ awito/script/script.js
@@ -3,12 +3,17 @@
 const modalAdd = document.querySelector('.modal__add'),
   addAd = document.querySelector('.add__ad'),
   modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-  modalSubmit = document.querySelector('.modal__submit');
+  modalSubmit = document.querySelector('.modal__submit'),
+  catalog = document.querySelector('.catalog'),
+  modalItem = document.querySelector('.modal__item ');
 
+
+// show modal add card
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
   modalBtnSubmit.disabled = true;
 });
+
 
 modalAdd.addEventListener('click', event => {
   // console.log(event);
@@ -18,5 +23,22 @@ modalAdd.addEventListener('click', event => {
     modalAdd.classList.add('hide');
     modalSubmit.reset();
   }
+});
 
+catalog.addEventListener('click', event => {
+  const target = event.target.closest('.card');
+
+  if (target) {
+    modalItem.classList.remove('hide');
+    console.log('okk');
+  }
+});
+
+document.addEventListener('keydown', event => {
+  // console.log(event);
+  if (event.keyCode == 27) {
+    console.log('ok');
+    modalItem.classList.add('hide');
+  }
+  // console.log(target);
 });
